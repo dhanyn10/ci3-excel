01c7d39bb7946cae5caa7c72336635076c696629
dhanyn10 <dhanyn.dhan@gmail.com>
2018-09-04 14:22:35 +0700

    menggunakan library form_validation

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
 	public function index()
 	{
 		$this->load->view('excel');
-	}
+    }
 }
