5113a772c9e09c4804cd3519e9695c178224153d
dhanyn10 <dhanyn.dhan@gmail.com>
2018-09-04 14:05:10 +0700

    ubah default controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['default_controller']    = 'home';
+$route['404_override']          = '';
+$route['translate_uri_dashes']  = FALSE;
